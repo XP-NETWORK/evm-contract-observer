46cc94bdbf8455e20c6bc77567b59dd62e79f5e5
AlexeyAdoniev <Alexeya@xp.network>
2022-05-16 14:46:07 +0300

    add 3 more collections

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,9 @@ app.listen(config.port || 3100, async () => {
 
   runWorker({contract: '0x0c5ab026d74c451376a4798342a685a0e99a5bee', timeout: .5, interval: 5 ,name: 'MachineFi NFT', symbol: 'MFI', chainId: '20' });
 
+  runWorker({contract: '0xbe6514fc4a702793e46fe3516fc70d160d13a463', timeout: 1, interval: 5 ,name: 'Iotexsons special', symbol: 'TIOSS', chainId: '20' });
+  runWorker({contract: '0x30582ede7fadeba4973dd71f1ce157b7203171ea', timeout: 2, interval: 5 ,name: 'Ucam Pioneer', symbol: 'UPT', chainId: '20' });
+  runWorker({contract: '0x55cbc794f7577e3d6b787c014a607c39373632eb', timeout: 2.5, interval: 5 ,name: 'Iotexsons avatar', symbol: 'TIOS', chainId: '20'});
 })
 
 
